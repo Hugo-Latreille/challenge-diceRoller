184a25014fa05fa1e0903495ceb99d10a10e65a8
hugo-latreille <hugo.latreille@gmail.com>
2022-03-10 17:02:21 +0100

    étape 3

--- js/app.js
+++ js/app.js
@@ -1,11 +1,3 @@
-var divDice = document.createElement("div");
-
-divDice.classList.add("dice");
-
-var player = document.getElementById("player");
-
-player.appendChild(divDice);
-
 function randomNumber(min, max) {
 	// entre 1 et 6
 	return Math.round(Math.random() * (max - min) + min);
@@ -14,7 +6,28 @@ function randomNumber(min, max) {
 function rollDice() {
 	var dice = randomNumber(1, 6);
 	console.log(dice);
+
+	var divDice = document.createElement("div");
+	divDice.classList.add("dice");
+	var player = document.getElementById("player");
+	player.appendChild(divDice);
 	divDice.style.backgroundPositionX = `${dice * 100}px`;
 }
 
-rollDice();
+function severalRolls() {
+	var combien = prompt("Combien de dés souhaitez-vous lancer ?");
+	if (combien === null) {
+		return;
+	}
+	var combienChiffre = parseInt(combien);
+	if (isNaN(combienChiffre)) {
+		alert("Veuillez entrer un chiffre");
+	}
+	console.log(combienChiffre);
+
+	for (var i = 0; i < combienChiffre; i++) {
+		rollDice();
+	}
+}
+
+severalRolls();
