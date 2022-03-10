ec14eb59767ecd7af775fd10f540cc57460f283a
hugo-latreille <hugo.latreille@gmail.com>
2022-03-10 16:31:34 +0100

    étape 2

--- js/app.js
+++ js/app.js
@@ -5,3 +5,16 @@ divDice.classList.add("dice");
 var player = document.getElementById("player");
 
 player.appendChild(divDice);
+
+function randomNumber(min, max) {
+	// entre 1 et 6
+	return Math.round(Math.random() * (max - min) + min);
+}
+
+function rollDice() {
+	var dice = randomNumber(1, 6);
+	console.log(dice);
+	divDice.style.backgroundPositionX = `${dice * 100}px`;
+}
+
+rollDice();
