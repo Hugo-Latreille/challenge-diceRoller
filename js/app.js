81fe3ef24031cccdd646993b807a6a70515c2083
hugo-latreille <hugo.latreille@gmail.com>
2022-03-11 13:58:42 +0100

    ajouts fonctionnalités eventListener -cours

--- js/app.js
+++ js/app.js
@@ -18,21 +18,33 @@ function rollDice(playerId) {
 		otherPlayer.appendChild(divDice);
 	}
 
-	divDice.style.backgroundPositionX = `${dice * 100}px`;
+	divDice.style.backgroundPositionX = `${dice * -100}px`;
+	// divDice.style.backgroundPositionX = dice * -100 + "px";
+}
+
+function eraseDices(userId) {
+	var playerBoardElement = document.querySelector(userId);
+	playerBoardElement.innerHTML = "";
 }
 
 function severalRolls() {
-	var combien = prompt("Combien de dés souhaitez-vous lancer ?");
-	if (combien === null) {
-		return;
-	}
-	var combienChiffre = parseInt(combien);
-	if (isNaN(combienChiffre)) {
-		alert("Veuillez entrer un chiffre");
-	}
-	console.log(combienChiffre);
+	// var combien = prompt("Combien de dés souhaitez-vous lancer ?");
+	// if (combien === null) {
+	// 	return;
+	// }
+	// var combienChiffre = parseInt(combien);
+	// if (isNaN(combienChiffre)) {
+	// 	alert("Veuillez entrer un chiffre");
+	// }
+	// console.log(combienChiffre);
 
-	for (var i = 0; i < combienChiffre; i++) {
+	eraseDices("#player");
+	eraseDices("#dealer");
+
+	var rangeElement = document.querySelector(".howMany");
+	var diceCount = rangeElement.value;
+
+	for (var i = 0; i < diceCount; i++) {
 		rollDice("dealer");
 		rollDice("player");
 	}
@@ -46,5 +58,24 @@ function adversary() {
 	app.appendChild(otherPlayer);
 }
 
+function updateRangeInputValue() {
+	var rangeInputElement = document.querySelector(".howMany");
+	var rangeValue = rangeInputElement.value;
+
+	// je recupère le span
+	var rangeValueElement = document.querySelector("#range-value");
+	// et je modifie son contenu pour afficher ce qui selectionné avec le slider
+	rangeValueElement.textContent = rangeValue;
+
+	severalRolls();
+}
+
 adversary();
-severalRolls();
+
+var btnElement = document.querySelector("#rollDicesBtn");
+btnElement.addEventListener("click", severalRolls);
+
+var rangeInputElement = document.querySelector(".howMany");
+// https://developer.mozilla.org/fr/docs/Web/API/HTMLElement/change_event
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/input_event
+rangeInputElement.addEventListener("input", updateRangeInputValue);
